696ed2f066afbfe0c8bf9ffa7a227b616bd02235
ZRELOLERZ <batmanbane4@gmail.com>
2021-06-12 22:57:33 -0400

    - added getAllEssayTypeQuestion for frontend

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { gql, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import Logo from './Logo';
 import InfoArea from './InfoArea';
-import { ALL_MULTIPLE_CHOICE_TYPE_QUESTION } from './schemas';
+import {
+  ALL_MULTIPLE_CHOICE_TYPE_QUESTION,
+  ALL_ESSAY_TYPE_QUESTION
+} from './schemas';
 
 function App() {
   const allMultipleChoiceTypeQuestionQuery = useQuery(
@@ -15,19 +18,44 @@ function App() {
   const allMultipleChoiceTypeQuestionQueryDataLoading =
     allMultipleChoiceTypeQuestionQuery.loading;
 
-  const [allMultipleChoiceTypeQuestionData, setAllMultipleChoiceTypeQuestion] =
-    useState([{}]);
+  const [
+    allMultipleChoiceTypeQuestionData,
+    setAllMultipleChoiceTypeQuestionData
+  ] = useState([{}]);
+
+  const allEssayTypeQuestionQuery = useQuery(ALL_ESSAY_TYPE_QUESTION);
+
+  const allEssayTypeQuestionQueryData = allEssayTypeQuestionQuery.data;
+
+  const allEssayTypeQuestionQueryDataLoading =
+    allMultipleChoiceTypeQuestionQuery.loading;
+
+  const [allEssayTypeQuestionData, setAllEssayTypeQuestionData] = useState([
+    {}
+  ]);
 
   useEffect(() => {
     if (
       !allMultipleChoiceTypeQuestionQueryDataLoading &&
       allMultipleChoiceTypeQuestionQueryData.getAllMultipleChoiceTypeQuestion
     ) {
-      setAllMultipleChoiceTypeQuestion(
+      setAllMultipleChoiceTypeQuestionData(
         allMultipleChoiceTypeQuestionQueryData.getAllMultipleChoiceTypeQuestion
       );
     }
   });
+
+  useEffect(() => {
+    if (
+      !allEssayTypeQuestionQueryDataLoading &&
+      allEssayTypeQuestionQueryData.getAllEssayTypeQuestion
+    ) {
+      setAllEssayTypeQuestionData(
+        allEssayTypeQuestionQueryData.getAllEssayTypeQuestion
+      );
+    }
+  });
+
   return (
     <div className="container h-screen mx-auto bg-darkGray p-5 font-sans">
       <Logo />
--- frontend/src/components/schemas.js
+++ frontend/src/components/schemas.js
@@ -13,5 +13,18 @@ const ALL_MULTIPLE_CHOICE_TYPE_QUESTION = gql`
   }
 `;
 
+const ALL_ESSAY_TYPE_QUESTION = gql`
+  query GetAllEssayTypeQuestion {
+    getAllEssayTypeQuestion {
+      ID
+      dateCreated
+      dateModified
+      questionYear
+      questionType
+      questionAuthor
+    }
+  }
+`;
+
 // eslint-disable-next-line import/prefer-default-export
-export { ALL_MULTIPLE_CHOICE_TYPE_QUESTION };
+export { ALL_MULTIPLE_CHOICE_TYPE_QUESTION, ALL_ESSAY_TYPE_QUESTION };
